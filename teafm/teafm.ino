f9462c9ae10108067c40c1aa0d90f5c4b7d6b9ff
Aryan Karamtoth <88923986+SpaciousCoder78@users.noreply.github.com>
2025-02-07 13:44:33 +0530

    Update teafm.ino

--- teafm/teafm.ino
+++ teafm/teafm.ino
@@ -1,9 +1,52 @@
+//including necessary libraries
+#include <Arduino.h>
+#include <Wire.h>
+#include <radio.h>
+#include <TEA5767.h>
+
+#define FIX_BAND RADIO_BAND_FM //fixing fm as band
+
+//default station is 91.10
+#define FIX_STATION 9110
+
+//object radio from TEA5767 class
+TEA5767 radio;
+
+uint8_t test1;
+byte test2;
+
+
 void setup() {
-  // put your setup code here, to run once:
+  //open serial port
+  Serial.begin(57600);
+  Serial.println("Radio...");
+  delay(200);
+
+  // Initialize the Radio 
+  radio.init();
+
+  // Enable information to the Serial port
+  radio.debugEnable();
+
+  // HERE: adjust the frequency to a local sender
+  radio.setBandFrequency(FIX_BAND, FIX_STATION); // hr3 nearby Frankfurt in Germany
+  radio.setVolume(2);
+  radio.setMono(false);
 
 }
 
+/// show the current chip data every 3 seconds.
 void loop() {
-  // put your main code here, to run repeatedly:
+  char s[12];
+  radio.formatFrequency(s, sizeof(s));
+  Serial.print("Station:"); 
+  Serial.println(s);
+  
+  Serial.print("Radio:"); 
+  radio.debugRadioInfo();
+  
+  Serial.print("Audio:"); 
+  radio.debugAudioInfo();
 
-}
+  delay(3000);
+} // loop
